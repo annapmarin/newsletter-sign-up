c221d129a842961afd8518df79dd84aaa13faaa5
annapmarin <annapratsmarin@gmail.com>
2023-05-27 23:23:21 +0200

    html ready

--- src/App.tsx
+++ src/App.tsx
@@ -5,42 +5,77 @@
 // () View the optimal layout for the interface depending on their device's screen size
 // () See hover and focus states for all interactive elements on the page
 
-import { useState } from "react"
+import { useState } from "react";
+import mobileCaption from "../src/assets/images/illustration-sign-up-mobile.svg";
+import trueIcon from "../src/assets/images/icon-success.svg";
 
 function App() {
-  const [submitted, setSubmitted] = useState(false)
-  const [validEmail, setValidEmail] = useState(false)
-  
-    // const handleSubmitted = () => {
-    //   if (validEmail) {
-    //     setSubmitted(true);
-    //   } else {
-    //     setSubmitted(false);
-    //   }
-    // };
+  const [submitted, setSubmitted] = useState(false);
+  const [validEmail, setValidEmail] = useState(false);
 
-    // const handleEmail = (email) => {
-      
-    // }
+  // const handleSubmitted = () => {
+  //   if (validEmail) {
+  //     setSubmitted(true);
+  //   } else {
+  //     setSubmitted(false);
+  //   }
+  // };
 
+  // const handleEmail = (email) => {
 
+  // }
 
   if (!submitted) {
-    return(
+    return (
       <>
-        
+        <main>
+          <img src={mobileCaption} alt="" />
+          <section>
+            <div>
+              <h1>Stay updated!</h1>
+              <p>Join 60,000+ product managers receiving monthly updates on:</p>
+              <ul>
+                <li>
+                  <img src={trueIcon} alt="" />
+                  Product discovery and building what matters
+                </li>
+                <li>
+                  <img src={trueIcon} alt="" />
+                  Measuring to ensure updates are a success
+                </li>
+                <li>
+                  <img src={trueIcon} alt="" />
+                  And much more!
+                </li>
+              </ul>
+            </div>
+            <form>
+              <span>Email adress</span>
+              <input type="text" id="email" placeholder="email@company.com" />
+              <button type="submit">Subscribe to monthly newsletter</button>
+            </form>
+          </section>
+        </main>
       </>
-    )
+    );
   } else {
     return (
       <>
-  
-        {/* Stay updated! Join 60,000+ product managers receiving monthly updates on: Product discovery and building what matters Measuring to ensure updates are a success And much more! Email address email@company.com Subscribe to monthly newsletter Thanks for subscribing! A confirmation email has been sent to ash@loremcompany.com. Please open it and click the button inside to confirm your subscription. Dismiss message */}
+        <main>
+          <section>
+            <img src={trueIcon} alt="" />
+            <h1>Thanks for subscribing!</h1>
+            <p>
+              A confirmation email has been sent to{" "}
+              <span>ash@loremcompany.com</span>. Please open it and click the
+              button inside to confirm your subscription.
+            </p>
+          </section>
+          <button>Dismiss message</button>
+        </main>
       </>
-    )
+    );
   }
+}
 
-  }
-
-
-export default App
+export default App;
